646ee4945c2edf7ba961c94d9bff82822cd2b33e
aidin-rezaei <Aydinrezaeefdr@gmail.com>
2023-02-17 05:20:23 -0600

    update usergetsupervisors.php

--- usergetsupervisors.php
+++ usergetsupervisors.php
@@ -2,38 +2,19 @@
 include('config.php');
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if ($_POST['username'] && $_SERVER['HTTP_AUTHORIZATION'] && $_POST['id']) {
-        $password = str_replace('Bearer ', '', $_SERVER['HTTP_AUTHORIZATION']);
-        $id = $_POST['id'];
-        $sql = "SELECT username,
-        email,
-        phone 
-        FROM supervisors WHERE password = '" . $password . "' AND email='" . $_POST['username'] . "'";
 
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $sql = "SELECT * FROM users WHERE supervisor='$id' ";
-                $result = $conn->query($sql);
-                // echo 1;
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        $room[] = $row;
-                        $rows['data']['posts'] = $room;
-                        $rows['data']['status'] = '1';
-                    }
-                } else {
-                    $rows['data']['status'] = '0';
-                    $rows['data']['error'] = $conn->error;
-                }
-                // }
-            }
-        } else {
-            $rows['data']['status'] = '0';
-            $rows['data']['error'] = $conn->error;
-        }
-        echo (json_encode($rows, 448));
+$sql = "SELECT username,id FROM supervisors  ";
+$result = $conn->query($sql);
+// echo 1;
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $room[] = $row;
+        $rows['data']['posts'] = $room;
+        $rows['data']['status'] = '1';
     }
+} else {
+    $rows['data']['status'] = '0';
+    $rows['data']['error'] = $conn->error;
 }
+
+echo (json_encode($rows, 448));
